4262d8ccef60acdfe2c2d73beca7fb9726eed6bf
Bryan España <esp21550@uvg.edu.gt>
2022-01-19 17:35:16 -0600

    Update de Vista 0.2

--- HT1/src/com/uvg/ayed/vista.java
+++ HT1/src/com/uvg/ayed/vista.java
@@ -13,12 +13,24 @@ public class vista {
     
     public void bienvenida(){
         System.out.println("----------RADIO----------\n"
-                        +  "1.Encender/Apagar Radio \n"
+                        +  "1. Encender/Apagar Radio \n"
                         +  "2. Cambia de AM a FM o AM\n" 
                         +  "3. Siguiente emisora\n" 
                         +  "4. Guardar Emisora (Maximo 12)\n" 
                         +  "5. Seleccionar Emisora Guardada (Del 1 al 12)\n" 
                         +  "6. Apagar el radio");
     }
+    public void Error(){
+        System.out.println("ERROR. intentelo de nuevo");
+    }
+
+    public void Encendido(){
+        System.out.println("El Radio Esta encendido");
+    }
+    
+    public void Apagado(){
+        System.out.println("El Radio Esta Apagado");
+    }
+    
 
 }
