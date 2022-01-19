dfbd4c6f8b6761f329ab67291fd64206ddbf3b02
Bryan España <esp21550@uvg.edu.gt>
2022-01-19 16:38:36 -0600

    Actualización de modulo 0.2

---
+++ HT1/src/com/uvg/ayed/Radio.java
@@ -0,0 +1,16 @@
+package com.uvg.ayed;
+
+/**
+ *
+ * @author Bryan
+ */
+public class Radio implements IRadio{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
