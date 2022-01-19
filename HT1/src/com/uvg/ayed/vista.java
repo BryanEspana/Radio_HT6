8a32be0c4d37ded04a8b3dfdca333df2e109b1a9
Bryan España <esp21550@uvg.edu.gt>
2022-01-19 17:09:56 -0600

    Implementación de Main, Vista y controlador (ACTUALIZADO)

---
+++ HT1/src/com/uvg/ayed/vista.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.uvg.ayed;
+
+/**
+ *
+ * @author Bryan
+ */
+public class vista {
+    
+
+}
